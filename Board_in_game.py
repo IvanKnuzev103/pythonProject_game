0b7e8ad050e61d1467d3eb9cb8a0c6d08c3cfc8c
ivan <ryzptdbdfy@mail.ru>
2023-12-18 23:12:00 +0300

    commit_4 сделал полность ресурсы кроме действия принадлежности его надо реолизовать после создания юнитов

--- Board_in_game.py
+++ Board_in_game.py
@@ -1,9 +1,10 @@
 import pygame
-
+import helpper_program
 import class_Main_building
 import class_all_objects
 import class_random_resurs_in_map
 import class_resurs_gold
+
 
 class Board:
     # создание поля
@@ -34,7 +35,7 @@ class Board:
     def get_click(self, mouse_pos, type_object):
         cell = self.get_cell(mouse_pos, type_object)
         self.on_click(cell)
-        pygame.draw.rect(self.screen_1, 'red', (cell[0] * 40,  cell[1] * 40, 40, 40), 5)
+        pygame.draw.rect(self.screen_1, 'red', (cell[0] * 40, cell[1] * 40, 40, 40), 5)
 
     def get_cell(self, mouse_pos, type_object='None_object'):
         x = mouse_pos[0] - self.left
@@ -53,51 +54,30 @@ class Board:
             self.parametrs = f'hp={100}, opportunities={None}, resurs_players={all_resyrs}'
             type_object = class_Main_building.Main_building(col, row, self.screen_1, self.parametrs)
 
-        elif 19 > col > 1 and 19 > row > 1:
+        elif (col != 0 and row != 0) or (col + 1 != self.width and row + 1 != self.height):
             Player = None
             self.parametrs = f'Affiliation_to_player={Player},type_resurs={None}, opportunities={None}'
+            helpper_program.drow_map()
             old_data = (open('map_game', 'r')).readlines()
             if (old_data[row][col]) == '.':
-                mode_str = old_data[row]
-                old_data[row] = (mode_str[:col] + 'g' + mode_str[col+1:len(mode_str)])
-                new_data = old_data
-                print(new_data)
-                file = open('map_game', 'w')
-                for elem in new_data:
-                    file.write(elem)
                 type_object = class_random_resurs_in_map.resurs(col, row, self.screen_1, self.parametrs)
             elif (old_data[row][col]) == 'g':
-                mode_str = old_data[row]
-                old_data[row] = (mode_str[:col] + 'w' + mode_str[col+1:len(mode_str)])
-                new_data = old_data
-                print(new_data)
-                file = open('map_game', 'w')
-                for elem in new_data:
-                    file.write(elem)
                 type_object = class_resurs_gold.resurs_gold(col, row, self.screen_1, self.parametrs)
             elif (old_data[row][col]) == 'w':
-                mode_str = old_data[row]
-                old_data[row] = (mode_str[:col] + 'i' + mode_str[col+1:len(mode_str)])
-                new_data = old_data
-                print(new_data)
-                file = open('map_game', 'w')
-                for elem in new_data:
-                    file.write(elem)
                 type_object = class_resurs_gold.resurs_gold(col, row, self.screen_1, self.parametrs)
             elif (old_data[row][col]) == 'i':
-                mode_str = old_data[row]
-                old_data[row] = (mode_str[:col] + '.' + mode_str[col+1:len(mode_str)])
-                new_data = old_data
-                print(new_data)
-                file = open('map_game', 'w')
-                for elem in new_data:
-                    file.write(elem)
                 type_object = class_resurs_gold.resurs_gold(col, row, self.screen_1, self.parametrs)
-
-
-
-
-        return (col, row, type_object, self.parametrs)
+            print(old_data)
+        return col, row, type_object, self.parametrs
 
     def on_click(self, cell_coords):
         print(cell_coords)
+
+# if (old_data[row][col]) == '.':
+#     mode_str = old_data[row]
+#     old_data[row] = (mode_str[:col] + 'g' + mode_str[col + 1:len(mode_str)])
+#     new_data = old_data
+#     print(new_data)
+#     file = open('map_game', 'w')
+#     for elem in new_data:
+#         file.write(elem)
--- helpper_program.py
+++ helpper_program.py
@@ -1,5 +1,31 @@
+import random
+
+
+
+
 def drow_map():
+    L = []
     for i in range(20):
-        print('.' * 30)
+        L.append((('.' + ' ') * 30).split())
+    for elem in L:
+        elem[random.randrange(0, 30)] = 'g'
+
+    for elem in L:
+        elem[random.randrange(0, 30)] = 'i'
+
+    for elem in L:
+        elem[random.randrange(0, 30)] = 'w'
+
+    print()
+    L[0] = (('@' + ' ') + ('.' + ' ') * 29).split()
+    L[19] = (('.' + ' ') * 29 + ('@' + ' ')).split()
+
+    file = open('map_game', 'w')
+    for elem in L:
+        file.write(''.join(elem) + '\n')
+
 
 drow_map()
+
+
+
